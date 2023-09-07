1f9bc8a743b459685ec99d151d3bf5ff0d68ab40
aeshevdaniyar <denaeshev@gmail.com>
2023-09-07 11:56:12 +0300

    remove unusable files

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -4,7 +4,10 @@ import { SearchFilterPokemons } from "@features/SearchFilterPokemons";
 export const Header = () => {
   return (
     <Box>
-      <Image mb={4} src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" />
+      <Image
+        mb={4}
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"
+      />
       <SearchFilterPokemons />
     </Box>
   );
